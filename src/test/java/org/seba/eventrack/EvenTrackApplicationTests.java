3281ccd98b14e564f064b0773278eba070d85505
Sebastien De Laet <delaet.sebastien93@gmail.com>
2025-02-26 13:51:33 +0100

    first commit

---
+++ src/test/java/org/seba/eventrack/EvenTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seba.eventrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
