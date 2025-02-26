4fc0254f580c3a1abcf91d8307d78d3444e5ae5f
Sebastien De Laet <delaet.sebastien93@gmail.com>
2025-02-26 16:40:59 +0100

    ajout SearchOperator SearchParam SearchSpecification + modif user

--- src/main/java/org/seba/eventrack/bll/services/UserService.java
+++ src/main/java/org/seba/eventrack/bll/services/UserService.java
@@ -1,4 +1,21 @@
 package org.seba.eventrack.bll.services;
 
+import org.seba.eventrack.dl.entities.User;
+import org.seba.eventrack.il.requests.SearchParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface UserService {
+
+    Page<User> getUsers(List<SearchParam<User>> searchParams, Pageable pageable);
+
+    User getUserByEmail(String email);
+
+    User saveUser(User user);
+
+    User updateUser(User user);
+
+    void deleteUser(Long id);
 }
--- src/main/java/org/seba/eventrack/dal/repositories/UserRepository.java
+++ src/main/java/org/seba/eventrack/dal/repositories/UserRepository.java
@@ -2,11 +2,12 @@ package org.seba.eventrack.dal.repositories;
 
 import org.seba.eventrack.dl.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
-
     Optional<User> findByEmail(String email);
+
 }
