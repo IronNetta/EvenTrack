0f5bda43fa51fea8f86b69ef63a9c3f85796a76b
Sebastien De Laet <delaet.sebastien93@gmail.com>
2025-03-03 13:50:36 +0100

    legere modif

--- src/main/java/org/seba/eventrack/api/controllers/EventController.java
+++ src/main/java/org/seba/eventrack/api/controllers/EventController.java
@@ -1,6 +1,8 @@
 package org.seba.eventrack.api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.seba.eventrack.api.models.CustomPage;
+import org.seba.eventrack.api.models.event.dtos.EventDto;
 import org.seba.eventrack.bll.services.EventService;
 import org.seba.eventrack.dl.entities.Event;
 import org.seba.eventrack.il.requests.SearchParam;
@@ -21,38 +23,40 @@ public class EventController {
 
     private final EventService eventService;
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ORGANIZER') or hasRole('PARTICIPANT')")
     @GetMapping
-    public ResponseEntity<Page<Event>> getAllEvents(
+    public ResponseEntity<CustomPage<EventDto>> getAllEvents(
             @RequestParam Map<String, String> params,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "id") String sort
     ) {
         List<SearchParam<Event>> searchParams = SearchParam.create(params);
-        Page<Event> events = eventService.findAll(searchParams, PageRequest.of(page - 1, size, Sort.by(Sort.Direction.ASC, sort)));
-        return ResponseEntity.ok(events);
+        Page<Event> events = eventService.findAll(
+                searchParams,
+                PageRequest.of(page - 1, size, Sort.by(Sort.Direction.ASC, sort)));
+        List<EventDto> dtos = events.getContent().stream()
+                .map(EventDto::fromEvent)
+                .toList();
+        CustomPage<EventDto> result = new CustomPage<>(dtos,events.getTotalPages(),events.getNumber() + 1);
+        return ResponseEntity.ok(result);
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ORGANIZER') or hasRole('PARTICIPANT')")
     @GetMapping("/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable Long id) {
         return ResponseEntity.ok(eventService.findById(id));
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ORGANIZER')")
+
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         return ResponseEntity.ok(eventService.save(event));
     }
 
-    @PreAuthorize("hasRole('ADMIN') or (hasRole('ORGANIZER') and #id == authentication.principal.id)")
     @PutMapping("/{id}")
     public ResponseEntity<Event> updateEvent(@PathVariable Long id, @RequestBody Event event) {
         return ResponseEntity.ok(eventService.update(event));
     }
 
-    @PreAuthorize("hasRole('ADMIN') or (hasRole('ORGANIZER') and #id == authentication.principal.id)")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEvent(@PathVariable Long id) {
         eventService.deleteById(id);
