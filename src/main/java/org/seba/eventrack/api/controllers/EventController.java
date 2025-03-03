b0841a2db88fd69538420d2b96b2381161843b87
Daniel Hajdini <danielhajdini@gmail.com>
2025-03-03 14:02:48 +0100

    premier commit

--- src/main/java/org/seba/eventrack/api/controllers/EventController.java
+++ src/main/java/org/seba/eventrack/api/controllers/EventController.java
@@ -1,14 +1,18 @@
 package org.seba.eventrack.api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.seba.eventrack.api.models.CustomPage;
+import org.seba.eventrack.api.models.event.dtos.EventDto;
 import org.seba.eventrack.bll.services.EventService;
 import org.seba.eventrack.dl.entities.Event;
+import org.seba.eventrack.dl.entities.User;
 import org.seba.eventrack.il.requests.SearchParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,20 +25,25 @@ public class EventController {
 
     private final EventService eventService;
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ORGANIZER') or hasRole('PARTICIPANT')")
     @GetMapping
-    public ResponseEntity<Page<Event>> getAllEvents(
+    public ResponseEntity<CustomPage<EventDto>> getAllEvents(
             @RequestParam Map<String, String> params,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "id") String sort
     ) {
         List<SearchParam<Event>> searchParams = SearchParam.create(params);
-        Page<Event> events = eventService.findAll(searchParams, PageRequest.of(page - 1, size, Sort.by(Sort.Direction.ASC, sort)));
-        return ResponseEntity.ok(events);
+        Page<Event> events = eventService.findAll(
+                searchParams,
+                PageRequest.of(page - 1, size, Sort.by(Sort.Direction.ASC, sort)));
+        List<EventDto> dtos = events.getContent().stream()
+                .map(EventDto::fromEvent)
+                .toList();
+        CustomPage<EventDto> result = new CustomPage<>(dtos,events.getTotalPages(),events.getNumber() + 1);
+        return ResponseEntity.ok(result);
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('ORGANIZER') or hasRole('PARTICIPANT')")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable Long id) {
         return ResponseEntity.ok(eventService.findById(id));
@@ -57,5 +66,17 @@ public class EventController {
     public ResponseEntity<Void> deleteEvent(@PathVariable Long id) {
         eventService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+
+    @PutMapping("/{id}/accept")
+    public ResponseEntity<Event> acceptEvent(@RequestBody Event event, @AuthenticationPrincipal User user, @PathVariable String id) {
+        return ResponseEntity.ok(eventService.validateEvent(event, user));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}/reject")
+    public ResponseEntity<Event> rejectEvent(@RequestBody Event event, @AuthenticationPrincipal User user, @PathVariable String id) {
+        return ResponseEntity.ok(eventService.validateEvent(event, user));
     }
 }
---
+++ src/main/java/org/seba/eventrack/api/models/event/dtos/EventDto.java
@@ -0,0 +1,16 @@
+package org.seba.eventrack.api.models.event.dtos;
+
+import org.seba.eventrack.dl.entities.Event;
+
+public record EventDto(
+        Long id,
+        String name,
+        String description
+){
+
+    public static EventDto fromEvent(Event event){
+        return new EventDto(event.getId(), event.getTitle(), event.getDescription());
+    }
+
+
+}
