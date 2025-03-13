da989113698e8ab0707cddcec53d2be9d391a439
DESKTOP-NIU3KDV\DEV13 <dussart.barnabe@gmail.com>
2025-03-13 13:29:14 +0100

    kokot

--- src/main/java/org/seba/eventrack/api/controllers/CertificateController.java
+++ src/main/java/org/seba/eventrack/api/controllers/CertificateController.java
@@ -2,6 +2,8 @@ package org.seba.eventrack.api.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.seba.eventrack.bll.services.CertificateService;
+import org.seba.eventrack.bll.services.EventService;
+import org.seba.eventrack.bll.services.UserService;
 import org.seba.eventrack.dl.entities.Certificate;
 import org.seba.eventrack.dl.entities.Event;
 import org.seba.eventrack.dl.entities.User;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CertificateController {
     private final CertificateService certificateService;
+    private final EventService eventService;
+    private final UserService userService;
 
     @PostMapping("/generate")
-    public ResponseEntity<Certificate> generateCertificate(@RequestParam User userId, @RequestParam Event eventId) {
-        return ResponseEntity.ok(certificateService.generateCertificate(userId, eventId));
+    public ResponseEntity<Certificate> generateCertificate(@RequestParam Long userId, @RequestParam Long eventId) {
+
+        return ResponseEntity.ok(certificateService.generateCertificate(userService.getUserById(userId), eventService.findById(eventId)));
     }
 }
