885dbeecc1ef1a43026f5d9d0aea9c3d043c615d
Sébastien De Laet <112700304+IronNetta@users.noreply.github.com>
2025-03-14 08:48:46 +0100

    fin

--- src/main/java/org/seba/eventrack/dl/enums/BadgeType.java
+++
@@ -1,8 +0,0 @@
-package org.seba.eventrack.dl.enums;
-
-public enum BadgeType {
-    PARTICIPANT_ASSIDU, // 5 événements assistés
-    ORGANISATEUR_CONFIRME, // 3 événements organisés
-    VIP, // Événement exclusif
-    EXPLORATEUR // Événements dans 3 villes différentes
-}
